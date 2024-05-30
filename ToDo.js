5ec04bac0dd05bd14c005b3b37107be98f5c2c07
MegiAlimucaj <147115624+MegiAlimucaj@users.noreply.github.com>
2024-05-30 16:12:08 -0700

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,7 +1,15 @@
-function Todo({todo,index,remove}){
-  function handle(){
-    console.log('Ping:',index);
-    remove(index);
-  }
-  return <div className="todo" onClick={handle}>{todo.text} (-)</div>
+function AddItemsToDoList({addToDoNew}){
+    const [value, setValue] = React.useState('');
+    const handleSubmit = e => {
+     e.preventDefault();
+     if(!value) return;
+     addToDoNew(value);
+     setValue('');
+    }
+    return (
+     //on submit triggers when user presses "enter" key from his/her computer keyboard
+     <form onSubmit={handleSubmit}>
+       <input type="text" className="addToDoItems" value={value} placeholder="add items..." onChange={e =>setValue(e.target.value)}/>
+     </form>
+    );
 }
